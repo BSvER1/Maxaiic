23e14250e7fcf318bd9f9f78158f6c6037f94f32
maxbestgames <maxbestgames@gmail.com>
2016-01-09 02:21:57 +1100

    controllers support

---
+++ Assets/Player2Mov.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player2Mov : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Transform>().position = (new Vector3(Input.GetAxis("Player2_Horizontal"), 0, Input.GetAxis("Player2_Vertical"))) + GetComponent<Transform>().position;
+    }
+}
