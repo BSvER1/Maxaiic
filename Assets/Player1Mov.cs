63ee466219f32def2788b54c729b06a41f52d9d0
maxbestgames <maxbestgames@gmail.com>
2016-01-10 20:10:23 +1100

    stars

--- Assets/Player1Mov.cs
+++ Assets/Player1Mov.cs
@@ -11,13 +11,21 @@ public class Player1Mov : MonoBehaviour {
 
     float shipAngle;
 
-    public float RotateSpeed;
+    public float accelerationMod = 1;
+
+    public float RotateSpeed = 3;
+
+    public float rotOffSet;
+
+    public float degreeJitterReduce = 6; //this should be 3 times smaller then the rotate speed, acording to Max's crack theeory
+
 
 
     // Use this for initialization
 void Start () {
-	
-	}
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.maxAngularVelocity = 1000;
+    }
 
     // Update is called once per frame
     void Update() {
@@ -26,34 +34,38 @@ void Start () {
 
         if (Input.GetAxis("Player1_Acceleration") > 0)
         {
-            rb.AddForce(transform.up * Input.GetAxis("Player1_Acceleration"), ForceMode.Force);
+            rb.AddForce(transform.right * Input.GetAxis("Player1_Acceleration") * accelerationMod, ForceMode.Force);
         }
 
         Vector2 joyPos = new Vector2(Mathf.Rad2Deg * Mathf.Asin(Input.GetAxis("Player1_Horizontal")), Mathf.Rad2Deg * Mathf.Asin(Input.GetAxis("Player1_Vertical")));
 
-        shipAngle = transform.rotation.eulerAngles.y;
+        shipAngle = transform.rotation.eulerAngles.y % 360 + 360;
 
 
 
         if (joyPos.magnitude > joystickMovementThreshold)
         {
-            joyAngle = -1 * Mathf.Rad2Deg * Mathf.Atan2(joyPos.x, joyPos.y) + 180;
+            joyAngle = (-1 * Mathf.Rad2Deg * Mathf.Atan2(joyPos.x, joyPos.y) + 180 + rotOffSet) % 360 + 360;
+
+            float differenceAngle = Mathf.Abs(shipAngle - joyAngle);
 
             Debug.Log(joyAngle + " " + shipAngle);
 
-            if (shipAngle != joyAngle)
+            if (differenceAngle > degreeJitterReduce && differenceAngle < 360 - degreeJitterReduce)
             {
 
 
                 if ((joyAngle - shipAngle + 360) % 360 > 180)
-                {
-                    rb.angularVelocity = new Vector3(0, -RotateSpeed, 0);
-                }
+                    rb.angularVelocity = new Vector3(0, Mathf.Clamp(-RotateSpeed * differenceAngle * 0.1f, -RotateSpeed, 0), 0);
+                else if (((joyAngle - shipAngle + 360) % 360 < 180))
+                    rb.angularVelocity = new Vector3(0, Mathf.Clamp(RotateSpeed * differenceAngle * 0.1f, 0, RotateSpeed), 0);
                 else
-                    rb.angularVelocity = new Vector3(0, RotateSpeed, 0);
+                    rb.angularVelocity = new Vector3(0, 0, 0);
                 //Rigidbody rb = GetComponent<Rigidbody>();
                 // rb.angularVelocity = new Vector3(rb.angularVelocity.x, Mathf.Clamp(transform.rotation.eulerAngles.y - joyAngle, -maxAngularVelY, maxAngularVelY), rb.angularVelocity.z);
             }
+            else
+                rb.angularVelocity = new Vector3(0, 0, 0);
         }
         else
         {
